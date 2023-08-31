cfe42609913883db48a93593bf0e8a737fa54725
Christian C. Berclaz <christian.berclaz@mac.com>
2023-08-31 19:28:15 +0200

    Experiment with the "hydrate.nvim" plugin.

---
+++ lua/plugins/hydate.lua
@@ -0,0 +1,9 @@
+return {
+  "stefanlogue/hydrate.nvim",
+  opts = {
+    minute_interval = 20,
+    -- Render style
+    -- "default", "minimal", "simple", "compact"
+    render_style = "compact",
+  },
+}
